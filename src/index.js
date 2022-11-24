048bc407aee903e8f354bdf8093e131c787931ba
Michał <bodysmic@gmail.com>
2022-11-24 13:18:33 +0100

    add redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import ReactDOM from 'react-dom';
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import App from './App';
-import './Styles/normalize.scss'
-import './Styles/global.scss'
+import './Styles/normalize.scss';
+import './Styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
-  <App />,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
   document.querySelector('#root')
 );
